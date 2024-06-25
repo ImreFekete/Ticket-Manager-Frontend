237aaf77bc8cfe9ce7f4d19be3048386cb43b4be
Fekete Imre <feketeimi12@gmail.com>
2024-06-25 12:22:36 +0200

    modify App.jsx add Header component to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from 'react';
 import { Outlet } from "react-router-dom";
+import Header from './Component/Header/Header';
+
 export const UserContext = createContext(null);
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
   return (
     <UserContext.Provider value={{ userId, setUserId}}>
       <div className="App">
+        <Header />
         <Outlet />
       </div>
     </UserContext.Provider>
